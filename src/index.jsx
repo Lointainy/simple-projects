35c2cefc7f642a5e92992477cf02cb0b30ac70c0
Eugene Kozakov <lointainyyy@gmail.com>
2022-09-02 13:28:25 +0300

    added font-awesome

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,12 @@ import './index.css'
 /* FONT */
 import '@fontsource/montserrat'
 
+/* ICONS */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faSearch)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
