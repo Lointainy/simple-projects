97d4daa63d1115e38e90670a0bd2cadde0f614d7
Eugene Kozakov <lointainyyy@gmail.com>
2022-09-05 12:01:26 +0300

    dynamic name project

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import { useParams } from 'react-router-dom'
 
 /* PROJECT */
@@ -6,7 +8,15 @@ import { WordCount } from '../projects/WordCount/WordCount'
 export const ProjectPage = () => {
   const { projectName } = useParams()
 
-  return (<div className="container my-20">
-    {projectName == 'WordCount' ? <WordCount></WordCount> : ''}
-  </div>)
+  const components = {
+    WordCount,
+  }
+
+  const ProjectComponent = components[projectName]
+
+  return (
+    <div className="container my-20">
+      <ProjectComponent />
+    </div>
+  )
 }
