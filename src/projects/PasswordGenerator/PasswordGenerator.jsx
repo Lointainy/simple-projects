84fc7c49f15dde134245a748c7e07a81a0e10ced
Eugene Kozakov <lointainyyy@gmail.com>
2022-09-05 13:04:14 +0300

    create basic layout

--- src/projects/PasswordGenerator/PasswordGenerator.jsx
+++ src/projects/PasswordGenerator/PasswordGenerator.jsx
@@ -1,3 +1,20 @@
+import { Button } from '@/components/Button'
+
 export const PasswordGenerator = () => {
-  return <>password</>
+  return (
+    <div className="password-gen">
+      <input
+        className="w-full my-10 bg-slate-800 outline-none text-green-100 text-2xl"
+        type="text"
+        name=""
+        id=""
+        placeholder="Generate password ..."
+        disabled
+      />
+      <div className="flex flex-wrap justify-between">
+        <Button name={'generate'} iconName={'arrows-rotate'}></Button>
+        <Button name={'copy'} iconName={'copy'}></Button>
+      </div>
+    </div>
+  )
 }
