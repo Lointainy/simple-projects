090c525c40385ebc7b4bff497bef72d168ee6e87
Eugene Kozakov <lointainyyy@gmail.com>
2022-09-09 14:23:55 +0300

    added new project

---
+++ src/projects/DebtBook/DebtBook.jsx
@@ -0,0 +1,11 @@
+export const DebtBook = () => {
+  return (
+    <div className="debt-book__fuild bg-slate-700 p-4 rounded-xl">
+      DebtBook
+      <div className="debt-book__header bg-slate-800 rounded-xl p-6 my-2 text-white flex flex-col">
+        Current Clients Debt:
+        <span>1000</span>
+      </div>
+    </div>
+  )
+}
