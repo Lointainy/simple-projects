aff574cbd5d77f523e4cb3368f22f8e3453b1302
Eugene Kozakov <lointainyyy@gmail.com>
2022-09-13 12:20:34 +0300

    added new project

--- src/projects/DebtBook/DebtBook.jsx
+++ src/projects/DebtBook/DebtBook.jsx
@@ -1,11 +1,13 @@
+import { Button } from '@/components/Button'
+
 export const DebtBook = () => {
   return (
     <div className="debt-book__fuild bg-slate-700 p-4 rounded-xl">
-      DebtBook
-      <div className="debt-book__header bg-slate-800 rounded-xl p-6 my-2 text-white flex flex-col">
+      <div className="debt-book__header bg-slate-800 rounded-xl p-6 my-2 text-slate-400 flex flex-col">
         Current Clients Debt:
-        <span>1000</span>
+        <span className="text-3xl font-black  text-teal-500">1000</span>
       </div>
+      <Button className="w-full" name={'add new'} iconName={'plus'}></Button>
     </div>
   )
 }
